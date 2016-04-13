9628bbfed71b9400c6d450f0fd0230efe0a4d075
Michael Marshall <EmployYeezy@Michaels-MacBook-Pro-4.local>
2016-04-13 13:48:46 -0700

    adding 2nd and 3rd Activities -- COMPILED

---
+++ adiProjectOne/app/src/main/java/com/example/employeezy/adiprojectone/BlankList.java
@@ -0,0 +1,14 @@
+package com.example.employeezy.adiprojectone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BlankList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank_list);
+
+    }
+}
--- adiProjectOne/app/src/main/java/com/example/employeezy/adiprojectone/MainActivity.java
+++ adiProjectOne/app/src/main/java/com/example/employeezy/adiprojectone/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.employeezy.adiprojectone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button newListButton;
+    Button viewListsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newListButton = (Button) findViewById(R.id.newList);
+        viewListsButton = (Button) findViewById(R.id.viewLists);
+
+
+        OnClickListener newListClickListener = new OnClickListener() {
+            public void onClick(View v) {
+                Intent listAdd = new Intent(MainActivity.this, BlankList.class);
+                startActivity(listAdd);
+
+            }
+        };
+
+        OnClickListener viewListsClickListener = new OnClickListener() {
+            public void onClick(View v) {
+                Intent viewLists = new Intent(MainActivity.this, ListViewer.class);
+                startActivity(viewLists);
+            }
+        };
+
+        newListButton.setOnClickListener(newListClickListener);
+        viewListsButton.setOnClickListener(viewListsClickListener);
+
     }
 }
+
